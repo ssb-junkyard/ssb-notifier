a0e5cf29ae4ae6d872b56650bf6460cdcb7b247c
Charles Lehner <cel@celehner.com>
2016-02-23 23:33:36 -0500

    Use freedesktop-notifications on Linux

---
+++ notifier.js
@@ -0,0 +1,28 @@
+module.exports = function init(appName, cb) {
+  switch (require('os').type()) {
+
+    case 'Linux':
+      var notifications = require('freedesktop-notifications')
+      var proc = require('child_process')
+      return notifications.init(function (err) {
+        if (err) return cb(err)
+          notifications.setAppName(appName)
+        cb(null, function (notif) {
+          var notification = notifications.createNotification({
+            summary: notif.title,
+            body: notif.message,
+            // https://developer.gnome.org/notification-spec/#hints
+            'desktop-entry': 'ssb-patchwork-electron'
+          })
+          notification.push()
+        })
+      })
+
+    default:
+      var notifier = require('node-notifier')
+      cb(null, function (notif) {
+        notif.name = appName
+        notifier.notify(notif)
+      })
+  }
+}
--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var path = require('path')
 var ssbKeys = require('ssb-keys')
 var appName = process.env.ssb_appname || 'ssb'
 var conf = require('ssb-config/inject')(appName)
-var notifier = require('node-notifier')
 var pull = require('pull-stream')
 
 ssbKeys.loadOrCreate(path.join(conf.path, 'secret'), function (err, keys) {
@@ -16,18 +15,22 @@ ssbKeys.loadOrCreate(path.join(conf.path, 'secret'), function (err, keys) {
     sbot.whoami(function (err, feed) {
       if (err) return cb(err)
       var keepalive = setInterval(sbot.whoami, 15e3)
-      showNotifications(sbot, feed.id, function (err) {
-        clearTimeout(keepalive)
-        sbot.close(err, function (err) {
-          if (err) throw err
-          // TODO: try reconnecting
+      require('./notifier')(appName, function (err, notify) {
+        if (err) throw err
+        showNotifications(sbot, notify, feed.id, function (err) {
+          clearTimeout(keepalive)
+          sbot.close(err, function (err) {
+            if (err) throw err
+            // TODO: try reconnecting
+          })
         })
       })
     })
   })
 })
 
-function showNotifications(sbot, feedId, cb) {
+
+function showNotifications(sbot, notify, feedId, cb) {
   pull(
     sbot.createLogStream({
       live: true,
@@ -35,10 +38,7 @@ function showNotifications(sbot, feedId, cb) {
       gte: Date.now()
     }),
     require('./notifications')(sbot, feedId),
-    pull.drain(function (notif) {
-      if (!notif) return
-      notif.name = appName
-      notifier.notify(notif)
-    }, cb)
+    pull.filter(Boolean),
+    pull.drain(notify, cb)
   )
 }
