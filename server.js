9d29c7ba5132a480e51b1c44f07744fb014cf863
Charles Lehner <cel@celehner.com>
2016-02-27 20:08:50 -0500

    Reconnect automatically

--- server.js
+++ server.js
@@ -4,24 +4,62 @@
 
 var path = require('path')
 var ssbKeys = require('ssb-keys')
+var ssbClient = require('ssb-client')
 var appName = process.env.ssb_appname || 'ssb'
 var conf = require('ssb-config/inject')(appName)
 var pull = require('pull-stream')
 
+function reconnecter(createClient, onConnection) {
+  var keepaliveInterval = 15e3
+  var rpc
+  var reconnectDelay
+  var awaitingReconnect
+
+  function keepalive() {
+    rpc.whoami(function (err, feed) {
+      if (err) connect()
+      else setTimeout(keepalive, keepaliveInterval)
+    })
+  }
+
+  function connect() {
+    reconnectDelay = 1e3
+    if (awaitingReconnect)
+      clearTimeout(awaitingReconnect)
+    function reconnect() {
+      createClient(function (err, _rpc) {
+        awaitingReconnect = null
+        if (err) {
+          awaitingReconnect = setTimeout(reconnect, reconnectDelay *= 1.618)
+        } else {
+          rpc = _rpc
+          setTimeout(keepalive, keepaliveInterval)
+          onConnection(null, rpc, connect)
+        }
+      })
+    }
+    reconnect()
+  }
+
+  connect()
+}
+
 ssbKeys.loadOrCreate(path.join(conf.path, 'secret'), function (err, keys) {
   if (err) throw err
-  require('ssb-client')(keys, conf, function (err, sbot) {
+  require('./notifier')(appName, function (err, notify) {
     if (err) throw err
-    sbot.whoami(function (err, feed) {
+
+    reconnecter(function (cb) {
+      ssbClient(keys, conf, cb)
+    }, function (err, sbot, reconnect) {
       if (err) throw err
-      var keepalive = setInterval(sbot.whoami, 15e3)
-      require('./notifier')(appName, function (err, notify) {
+
+      sbot.whoami(function (err, feed) {
         if (err) throw err
         showNotifications(sbot, notify, feed.id, function (err) {
-          clearTimeout(keepalive)
           sbot.close(err, function (err) {
             if (err) throw err
-            // TODO: try reconnecting
+            reconnect()
           })
         })
       })
