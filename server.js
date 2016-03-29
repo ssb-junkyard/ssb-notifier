3924351a74f99e03b74547ff6250679faee5a6c2
Paul Frazee <pfrazee@gmail.com>
2016-03-29 14:55:38 -0500

    rewrite as a scuttlebot plugin

--- server.js
+++ server.js
@@ -2,49 +2,29 @@
 ':' //; exec "$(which nodejs || which node)" "$0" "$@"
 // vi: ft=javascript
 
-var lastTime = Date.now()
-
-var path = require('path')
-var ssbKeys = require('ssb-keys')
-var ssbClient = require('ssb-client')
 var appName = process.env.ssb_appname || 'ssb'
-var conf = require('ssb-config/inject')(appName)
 var pull = require('pull-stream')
 
-ssbKeys.loadOrCreate(path.join(conf.path, 'secret'), function (err, keys) {
-  if (err) throw err
-  require('./notifier')(appName, function (err, notify) {
-    if (err) throw err
-
-    require('ssb-reconnect')(function (cb) {
-      ssbClient(keys, conf, cb)
-    }, function (err, sbot, reconnect) {
+module.exports = {
+  name: 'notifier',
+  version: '1.0.0',
+  manifest: {},
+  permissions: {
+    master: {allow: []}
+  },
+  init: function (sbot, config) {
+    require('./notifier')(appName, function (err, notify) {
       if (err) throw err
-
-      sbot.whoami(function (err, feed) {
-        if (err) throw err
-        showNotifications(sbot, notify, feed.id, function (err) {
-          lastTime = Date.now()
-          sbot.close(err, function (err) {
-            if (err) throw err
-            reconnect()
-          })
-        })
-      })
+      pull(
+        sbot.createLogStream({
+          live: true,
+          reverse: true,
+          gte: Date.now()
+        }),
+        require('./notifications')(sbot, sbot.id),
+        pull.filter(Boolean),
+        pull.drain(notify)
+      )
     })
-  })
-})
-
-
-function showNotifications(sbot, notify, feedId, cb) {
-  pull(
-    sbot.createLogStream({
-      live: true,
-      reverse: true,
-      gte: lastTime
-    }),
-    require('./notifications')(sbot, feedId),
-    pull.filter(Boolean),
-    pull.drain(notify, cb)
-  )
+  }
 }
