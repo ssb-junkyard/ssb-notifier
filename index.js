ce565e5adb2687320cb02670cfcb1a9cf3afd231
cel <cel@f/6sQ6d2CMxRUhLpspgGIulDxDCwYD7DzFzPNr7u5AU=.ed25519>
2016-11-18 23:59:05 -0500

    Add rpc command

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ var pull = require('pull-stream')
 module.exports = {
   name: 'notifier',
   version: '1.0.0',
-  manifest: {},
-  permissions: {
-    master: {allow: []}
+  manifest: {
+    notify: 'sync'
   },
   init: function (sbot, config) {
-    require('./notifier')(appName, function (err, notify) {
+    var notify
+    require('./notifier')(appName, function (err, _notify) {
       if (err) return console.error('[notifier]', err.message || err)
+      notify = _notify
       pull(
         sbot.createLogStream({old: false}),
         require('./notifications')(sbot, sbot.id),
@@ -19,5 +20,12 @@ module.exports = {
         })
       )
     })
+
+    return {
+      notify: function (notification) {
+        if (notify) notify(notification)
+        else console.log('[notifier]', notification)
+      }
+    }
   }
 }
